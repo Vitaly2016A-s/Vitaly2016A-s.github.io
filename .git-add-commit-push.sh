72e4ee19288f8488b8cc7fbbcb6948ae858f057f
Vitaly A.92 <goldfresh@yandex.ru>
2019-02-10 02:19:38 +0300

    git-bash modifications

--- .git-add-commit-push.sh
+++ .git-add-commit-push.sh
@@ -1,12 +1,18 @@
-
+echo "RUN Auto git add + git commit + git push..."
 git status
 
 echo "Are you want to add changes? "
-
 read add_confirm
 
-if ( ( "$add_confirm" == "Y" ) || ( "$add_confirm" == "y" ) ); then
+if [ $add_confirm == 'y' || $add_confirm == 'Y' ]
+then
 	git add ./
+	echo "Type Commit Name: "
+	read commit_name
+	echo "Type a Commit Description: "
+	read commit_description
+	git commit -m $commit_name -m $commit_description
+	git push -u my-hosting master
 else
 	echo "STOPPED!"
 fi
