700c00d86cc65ecbbf4439e9c3d5d825db2d260a
Vitaly2016A-S <goldfresh@yandex.ru>
2019-07-09 18:17:25 +0300

    Yoga Web Site Portfolio

---
+++ GloAcademy/Portfolio/yoga/js/bundle/script.js
@@ -0,0 +1,39 @@
+/*
+* @Author: User
+* @Date:   2018-06-18 05:19:57
+* @Last Modified by:   User
+* @Last Modified time: 2018-06-26 19:41:48
+*/
+
+window.addEventListener('DOMContentLoaded', function() {
+
+
+// let ajax = require('../parts/ajax.js')
+
+let calc = require('../parts/calc.js')
+
+let modal = require('../parts/modal.js')
+
+let form = require('../parts/form.js')
+
+let slider = require('../parts/slider.js')
+
+let tab = require('../parts/tab.js')
+
+let timer = require('../parts/timer.js')
+
+// ajax();
+
+calc();
+
+modal();
+
+form();
+
+slider();
+
+tab();
+
+timer();
+
+});
