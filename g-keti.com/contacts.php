b8afb9807b690ed3e8698818b39560dde66ab878
Vitaly2016A-S <goldfresh@yandex.ru>
2019-11-05 23:33:30 +0300

    G-Keti WebSite Init

---
+++ g-keti.com/contacts.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="icon" href="./img/favicon.png" sizes="32x32" type="image/png">
+  <link rel="stylesheet" href="./css/style.css">
+  <title>G-Keti Official | Туры в Грузию | Контакты</title>
+</head>
+<body>
+<!-- HEADER -->
+<header class="header-page">
+  <div class="header-logo">
+    <div class="header-logo--pic">
+      <a href="./index.php" title="Главная"><img src="./img/logo.png" alt="G Keti"></a>
+    </div>
+  </div>
+  <div class="header-menu">
+    <ul>
+      <li><a href="./about.php" title="Обо мне">Обо мне</a></li>
+      <li><a href="#">Контакты</a></li>
+    </ul>
+  </div>
+</header>
+<!-- MAIN -->
+<main class="main-page">
+  <div class="container-page">
+    <h2>Мои контакты</h2>
+    <p>Здесь будут мои контакты WhatsApp, Instagram и Facebook</p>
+  </div>
+</main>
+<!-- FOOTER -->
+<footer class="footer-page">
+  <div class="footer-page--top">
+  <div class="footer-page--bottom">
+    <div class="footer-page-bottom--copyrights">
+      <code>© 2019 G-Keti. All Rights Reserved</code>
+    </div>
+  </div>
+</footer>
+
+  <script src="jquery-3.4.1.min.js"></script>
+  <script src="./js/main.js"></script>
+</body>
+</html>
---
+++ g-keti.com/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="icon" href="./img/favicon.png" sizes="32x32" type="image/png">
+  <link rel="stylesheet" href="./css/style.css">
+  <title>G-Keti Official | Туры в Грузию | Главная</title>
+</head>
+<body>
+
+<!-- HEADER -->
+<header class="header-page">
+  <div class="header-logo">
+    <div class="header-logo--pic">
+      <a href="#"><img src="./img/logo.png" alt="G Keti"></a>
+    </div>
+  </div>
+  <div class="header-menu">
+    <ul>
+      <li><a href="./about.php" title="Обо мне">Обо мне</a></li>
+      <li><a href="./contacts.php" title="Контакты">Контакты</a></li>
+    </ul>
+  </div>
+</header>
+
+<!-- MAIN -->
+<main class="main-page">
+  <div class="container-page">
+    <h2>Кетеван Габаташвили</h2>
+    <p>Туры в Грузию</p>
+  </div>
+</main>
+
+<!-- FOOTER -->
+<footer class="footer-page">
+  <div class="footer-page--top">
+  <div class="footer-page--bottom">
+    <div class="footer-page-bottom--copyrights">
+      <code>© 2019 G-Keti. All Rights Reserved</code>
+    </div>
+  </div>
+</footer>
+
+  <script src="jquery-3.4.1.min.js"></script>
+  <script src="./js/main.js"></script>
+</body>
+</html>
